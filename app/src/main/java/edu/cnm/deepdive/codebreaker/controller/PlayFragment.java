31d54a8fcb5e6e4babb44aceef21cc446bc0d906
nmitchell313 <nmitchell11@cnm.edu>
2021-10-25 15:11:41 -0600

    Layout & RecyclerView adapter (in progress).

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/PlayFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/PlayFragment.java
@@ -9,37 +9,43 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.codebreaker.adapter.GuessItemAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentPlayBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
 import org.jetbrains.annotations.NotNull;
 
 public class PlayFragment extends Fragment {
 
-    private MainViewModel viewModel;
-    private FragmentPlayBinding binding;
+  private MainViewModel viewModel;
+  private FragmentPlayBinding binding;
 
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentPlayBinding.inflate(inflater, container, false);
-        binding.submit.setOnClickListener((v) ->
-                viewModel.submitGuess(binding.guess.getText().toString().trim()));
-        return binding.getRoot();
-    }
+  public View onCreateView(
+      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    binding = FragmentPlayBinding.inflate(inflater, container, false);
+    binding.submit.setOnClickListener((v) ->
+        viewModel.submitGuess(binding.guess.getText().toString().trim()));
+    return binding.getRoot();
+  }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        viewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
-        viewModel.getThrowable().observe(getViewLifecycleOwner(), (throwable) -> {
-            if (throwable != null) {
-                Toast.makeText(getContext(), "throwable.getMessage()", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+    viewModel.getThrowable().observe(getViewLifecycleOwner(), (throwable) -> {
+      if (throwable != null) {
+        Toast.makeText(getContext(), "throwable.getMessage()", Toast.LENGTH_LONG).show();
+      }
+    });
+    viewModel.getGame().observe(getViewLifecycleOwner(), (game) -> {
+      GuessItemAdapter adapter = new GuessItemAdapter(getContext(), game.getGuesses());
+      binding.guesses.setAdapter(adapter);
+      binding.guessContainer.setVisibility(game.isSolved() ? View.GONE : View.VISIBLE);
+    });
+  }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
 }
