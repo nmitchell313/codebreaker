abf917c19c3cbe2afdf96947bd875c9968214213
nmitchell313 <nmitchell11@cnm.edu>
2021-10-26 15:16:08 -0600

    filter & new game working; changed label.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/PlayFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/PlayFragment.java
@@ -17,6 +17,7 @@ import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.GuessItemAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentPlayBinding;
+import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
 import java.util.Locale;
 import org.jetbrains.annotations.NotNull;
@@ -42,6 +43,7 @@ public class PlayFragment extends Fragment implements InputFilter {
     binding = FragmentPlayBinding.inflate(inflater, container, false);
     binding.submit.setOnClickListener((v) ->
         viewModel.submitGuess(binding.guess.getText().toString().trim()));
+    binding.guess.setFilters(new InputFilter[]{this});
     return binding.getRoot();
   }
 
@@ -54,15 +56,7 @@ public class PlayFragment extends Fragment implements InputFilter {
         Toast.makeText(getContext(), "throwable.getMessage()", Toast.LENGTH_LONG).show();
       }
     });
-    viewModel.getGame().observe(getViewLifecycleOwner(), (game) -> {
-      GuessItemAdapter adapter = new GuessItemAdapter(getContext(), game.getGuesses());
-      binding.guesses.setAdapter(adapter);
-      binding.guessContainer.setVisibility(game.isSolved() ? View.GONE : View.VISIBLE);
-      codeLength = game.getLength();
-      pool = game.getPool();
-      illegalCharacters = String.format(ILLEGAL_CHARACTERS_FORMAT, pool);
-      // TODO Enforce submit conditions.
-    });
+    viewModel.getGame().observe(getViewLifecycleOwner(), this::update);
   }
 
   @Override
@@ -76,6 +70,7 @@ public class PlayFragment extends Fragment implements InputFilter {
     boolean handled;
     if (item.getItemId() == R.id.new_game) {
       handled = true;
+      binding.guess.getText().clear();
       viewModel.startGame();
     } else {
       handled = super.onOptionsItemSelected(item);
@@ -104,7 +99,21 @@ public class PlayFragment extends Fragment implements InputFilter {
         modifiedSource.substring(0, modifiedSource.length() - (builder.length() - codeLength));
   }
   int newLength = dest.length() - (dend - dstart) + modifiedSource.length();
-  binding.submit.setEnabled(newLength == codeLength);
-  return modifiedSource;
+    checkSubmitConditions(newLength);
+    return modifiedSource;
+  }
+
+  private void update(Game game) {
+    GuessItemAdapter adapter = new GuessItemAdapter(getContext(), game.getGuesses());
+    binding.guesses.setAdapter(adapter);
+    binding.guessContainer.setVisibility(game.isSolved() ? View.GONE : View.VISIBLE);
+    codeLength = game.getLength();
+    pool = game.getPool();
+    illegalCharacters = String.format(ILLEGAL_CHARACTERS_FORMAT, pool);
+    checkSubmitConditions(binding.guess.getText().toString().trim().length());
+  }
+
+  private void checkSubmitConditions(int length) {
+    binding.submit.setEnabled(length == codeLength);
   }
 }
