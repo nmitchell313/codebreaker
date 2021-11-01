04ebc3ac503c868c550f09a38f13d1d22121106f
nmitchell313 <nmitchell11@cnm.edu>
2021-11-01 16:57:02 -0600

    added database classes.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/GameRepository.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GameRepository.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive.codebreaker.service;
 
+import androidx.annotation.NonNull;
+import edu.cnm.deepdive.codebreaker.model.dao.GameDao;
+import edu.cnm.deepdive.codebreaker.model.dao.GuessDao;
 import edu.cnm.deepdive.codebreaker.model.entity.Game;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
 import io.reactivex.Single;
@@ -7,44 +10,64 @@ import io.reactivex.schedulers.Schedulers;
 
 public class GameRepository {
 
-    private final WebServiceProxy proxy;
+  private final WebServiceProxy proxy;
+  private final GameDao gameDao;
+  private final GuessDao guessDao;
 
-    public GameRepository() {
-        proxy = WebServiceProxy.getInstance();
-    }
+  public GameRepository() {
+    proxy = WebServiceProxy.getInstance();
+    CodebreakerDatabase database = CodebreakerDatabase.getInstance();
+    gameDao = database.getGameDao();
+    guessDao = database.getGuessDao();
+  }
 
-    public Single<Game> startGame(String pool, int length) {
-        return Single
-                .fromCallable(() -> {
-                    Game game = new Game();
-                    game.setPool(pool);
-                    game.setLength(length);
-                    return game;
-                })
-                .flatMap(proxy::startGame)
-                .subscribeOn(Schedulers.io());
-    }
+  public Single<Game> startGame(String pool, int length) {
+    return Single
+        .fromCallable(() -> {
+          Game game = new Game();
+          game.setPool(pool);
+          game.setLength(length);
+          return game;
+        })
+        .flatMap(proxy::startGame)
+        .subscribeOn(Schedulers.io());
+  }
 
-    public Single<Game> submitGuess(Game game, String text) {
-        return Single
-                .fromCallable(() -> {
-                    Guess guess = new Guess();
-                    guess.setText(text);
-                    return guess;
-                })
-                .flatMap((guess) -> proxy.submitGuess(guess, game.getServiceKey()))
-                .map((guess) -> {
-                    game.getGuesses().add(guess);
-                    game.setSolved(guess.isSolution());
-                    return game;
-                })
-//            .flatMap((g) -> {
-//             if (g.isSolved()) {
-//               // use DOA to write Game and Guesses to database.
-//             }
-//             return g;
-//            })
-                .subscribeOn(Schedulers.io());
+  public Single<Game> submitGuess(Game game, String text) {
+    return Single
+        .fromCallable(() -> {
+          Guess guess = new Guess();
+          guess.setText(text);
+          return guess;
+        })
+        .flatMap((guess) -> proxy.submitGuess(guess, game.getServiceKey()))
+        .map((guess) -> {
+          game.getGuesses().add(guess);
+          game.setSolved(guess.isSolution());
+          return game;
+        })
+        .flatMap(this::insertGameWithGuesses)
+        .subscribeOn(Schedulers.io());
 
-    }
+  }
+
+  @NonNull
+  private Single<Game> insertGameWithGuesses(Game game) {
+    return (game.isSolved())
+      ? gameDao
+          .insert(game)
+          .map((id) -> {
+            game.setId(id);
+            for (Guess guess :
+                game.getGuesses()) {
+              guess.setGameId(id);
+            }
+            return game;
+          })
+          .flatMap((g2) -> guessDao
+              .insert(g2.getGuesses())
+              // TODO Invoke Guess.setId for all the guesses.
+        .map((ids) -> g2))
+    : Single.just(game);
+  }
 }
